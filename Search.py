77fb938e12e7cbc81e6dc4011e34e7550d99e1be
mikkp17 <mikkeleigner@gmail.com>
2019-11-04 13:56:16 +0100

    Update Search.py

--- Search.py
+++ Search.py
@@ -36,9 +36,9 @@ class Node:
 def generic_search():
     """The generic search algorithm. The algorithm starts with an initial state and builds a fringe, and then for
     every item in the fringe, it will explore child nodes until a solution is found."""
-
     # The initial node gets built from a map read from a text file
     initial_node = Node(generate_map())
+    goal_positions = find_goal_positions(initial_node.state)
 
     # The initial node gets inserted into the fringe to start the algorithm
     fringe = []
@@ -51,21 +51,21 @@ def generic_search():
         node = remove_first(fringe)
 
         # Check if the nodes state is equal to the goal state
-        if goal_reached(node.state):
+        if goal_reached(node.state, goal_positions):
             return node.path()
 
         # Find the children of the node and store them in the fringe
-        children = expand(node)
+        children = expand(node, goal_positions)
         fringe = insert_all(children, fringe)
 
 
-def expand(parent):
+def expand(parent, goal_positions):
     """Expands the current node and returns a list of children (successors)"""
     children = []
     actions = allowed_actions(parent.state)
     for action in actions:
         parent_state = copy.deepcopy(parent.state)
-        next_state = do_action(parent_state, action)
+        next_state = do_action(parent_state, action, goal_positions)
         child = Node(next_state, parent, action, parent.path_cost + 1)
         children = insert_into(child, children)
     return children
@@ -94,30 +94,30 @@ def allowed_actions(state):
     allowed = []
     i, j = find_current_pos(state)
 
-    if state[i - 1][j] == '.':
+    if state[i - 1][j] == '.' or state[i - 1][j] == 'G':
         allowed.append('u')
-    elif state[i - 1][j] == 'J' and state[i - 2][j] == '.':
+    elif state[i - 1][j] == 'J' and state[i - 2][j] == '.' or state[i - 1][j] == 'J' and state[i - 2][j] == 'G':
         allowed.append('U')
 
     if state[i][j + 1] == '.':
         allowed.append('r')
-    elif state[i][j + 1] == 'J' and state[i][j + 2] == '.':
+    elif state[i][j + 1] == 'J' and state[i][j + 2] == '.' or state[i][j + 1] == 'J' and state[i][j + 2] == 'G':
         allowed.append('R')
 
     if state[i + 1][j] == '.':
         allowed.append('d')
-    elif state[i + 1][j] == 'J' and state[i + 2][j] == '.':
+    elif state[i + 1][j] == 'J' and state[i + 2][j] == '.' or state[i + 1][j] == 'J' and state[i + 2][j] == 'G':
         allowed.append('D')
 
     if state[i][j - 1] == '.':
         allowed.append('l')
-    elif state[i][j - 1] == 'J' and state[i][j - 2] == '.':
+    elif state[i][j - 1] == 'J' and state[i][j - 2] == '.' or state[i][j - 1] == 'J' and state[i][j - 2] == 'G':
         allowed.append('L')
 
     return allowed
 
 
-def do_action(state, action):
+def do_action(state, action, goal_positions):
     """Executes a given action on a given state, returning the resulting state
     MAKE THIS WORK WITH G AS WELL"""
     i, j = find_current_pos(state)
@@ -127,36 +127,24 @@ def do_action(state, action):
         pushing = False
     if action.lower() == 'u':
         if pushing:
-            old_crate_pos = state[i - 2][j]
             state[i - 2][j] = 'J'
-            state[i - 1][j] = old_crate_pos
-        old_pos = state[i - 1][j]
         state[i - 1][j] = 'M'
-        state[i][j] = old_pos
     if action.lower() == 'r':
         if pushing:
-            old_crate_pos = state[i][j + 2]
             state[i][j + 2] = 'J'
-            state[i][j + 1] = old_crate_pos
-        old_pos = state[i][j + 1]
         state[i][j + 1] = 'M'
-        state[i][j] = old_pos
     if action.lower() == 'd':
         if pushing:
-            old_crate_pos = state[i + 2][j]
             state[i + 2][j] = 'J'
-            state[i + 1][j] = old_crate_pos
-        old_pos = state[i + 1][j]
         state[i + 1][j] = 'M'
-        state[i][j] = old_pos
     if action.lower() == 'l':
         if pushing:
-            old_crate_pos = state[i][j - 2]
             state[i][j - 2] = 'J'
-            state[i][j - 1] = old_crate_pos
-        old_pos = state[i][j - 1]
         state[i][j - 1] = 'M'
-        state[i][j] = old_pos
+    if (i, j) in goal_positions.values():
+        state[i][j] = 'G'
+    else:
+        state[i][j] = '.'
     return state
 
 
@@ -172,9 +160,26 @@ def find_current_pos(state):
     return -1, -1
 
 
-def goal_reached(state):
+def find_goal_positions(state):
+    goal_positions = {}
+    i = 0
+    for line in state:
+        j = 0
+        for char in line:
+            if char == 'G':
+                goal_positions[len(goal_positions)] = i, j
+            j += 1
+        i += 1
+    return goal_positions
+
+
+def goal_reached(state, goal_positions):
     """Returns true if the current state is the goal state, otherwise false"""
-    if state[7][2] == 'J' and state[9][2] == 'M':
+    lst = []
+    for goal in goal_positions.values():
+        i, j = goal
+        lst.append(state[i][j])
+    if all('J' in state for state in lst):
         return True
 
 
