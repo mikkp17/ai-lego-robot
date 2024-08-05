e5e379520c8cf71b2ce2451e68e85b76188ad380
Frederik Fabricius <frederikogfam@hotmail.com>
2024-08-05 12:21:11 +0200

    Update Search.py så det virker.

--- Search.py
+++ Search.py
@@ -1,47 +1,55 @@
-import copy  # Used to deepcopy our 2D array of state
 import heapq
-import sys
+from sys import maxsize
 
 
 class Node:
-    """A Node is a representation of an object in the tree. In Sokoban, each Node represents a state of the game,
-    and each of those nodes holds information about their parent nodes (previous moves) as well as what action they
-    did to get there from their parent node. A Node also keeps track of how expensive the path has been so far,
-    and how deep down the tree it is (how many moves it took to get there). """
 
-    def __init__(self, state, parent=None, action=None, path_cost=0):
-        """Create a search tree Node, derived from a parent by an action."""
-        self.state = state
+    competition_map = ""
+
+    def __init__(self, robot_position: tuple[int, int], crate_positions: list[tuple[int, int]], parent=None, action=None, path_cost=0, competition_map=None):
+        self.robot_position = robot_position
+        self.crate_positions = crate_positions
         self.parent = parent
         self.action = action
         self.path_cost = path_cost
         self.depth = 0
         self.manhattan_distance = -1
         self.boxes_not_in_goal = -1
+
         if parent:
-            self.depth = parent.depth + 1
+            self.depth: int = parent.depth + 1
+
+        if competition_map:
+            Node.competition_map = competition_map
 
     def __lt__(self, other):
+        assert isinstance(other, Node)
         return (self.boxes_not_in_goal, self.manhattan_distance) < (other.boxes_not_in_goal, other.manhattan_distance)
 
     def __repr__(self):
-        return "<Node {}>".format(self.state)
+        return f"<Node robot={self.robot_position}, crates={self.crate_positions}>"
 
-    def set_manhattan_distance(self, distance):
+    def set_manhattan_distance(self, distance: int):
+        assert isinstance(distance, int)
         self.manhattan_distance = distance
 
-    def get_manhattan_distance(self):
+    def get_manhattan_distance(self) -> int:
         return self.manhattan_distance
 
     def set_boxes_not_in_goal(self, boxes):
+        assert isinstance(boxes, int)
         self.boxes_not_in_goal = boxes
 
-    def get_boxes_not_in_goal(self):
+    def get_boxes_not_in_goal(self) -> int:
         return self.boxes_not_in_goal
 
-    def get_action(self):
+    def get_action(self) -> str | None:
         """Returns the action of this Node (Can be none)"""
         return self.action
+
+    def __eq__(self, other):
+        assert isinstance(other, Node)
+        return self.robot_position == other.robot_position and self.crate_positions == other.crate_positions
 
     def path(self):
         """Returns a list of nodes that form the path from root to current node"""
@@ -51,71 +59,75 @@ class Node:
             node = node.parent
         return list(reversed(path_back))
 
+    def __hash__(self):
+        return hash((self.robot_position, *self.crate_positions))
+
 
 def search():
     """The generic search algorithm. The algorithm starts with an initial state and builds a fringe, and then for
     every item in the fringe, it will explore child nodes until a solution is found."""
     # The initial node gets built from a map read from a text file
-    initial_node = Node(generate_map())
-    crate_positions = find_crate_positions(initial_node.state)
-    goal_positions = find_goal_positions(initial_node.state)
-    initial_node.set_manhattan_distance(calculate_manhattan_distance(crate_positions, goal_positions, initial_node))
-    initial_node.set_boxes_not_in_goal(calculate_boxes_not_in_goal(crate_positions, goal_positions, initial_node))
-    visited = []
+
+    the_map, robot_position, crate_positions = generate_map()
+    init_node = Node(robot_position, crate_positions, competition_map=the_map)
+
+    goal_positions = find_goal_positions(init_node.competition_map)
+    init_node.set_manhattan_distance(
+        calculate_manhattan_distance(goal_positions, init_node))
+    init_node.set_boxes_not_in_goal(
+        calculate_boxes_not_in_goal(goal_positions, init_node))
+
+    visited = {}
 
     # The initial node gets inserted into the fringe to start the algorithm
-    fringe = []
+    fringe: list[Node] = []
     heapq.heapify(fringe)
-    heapq.heappush(fringe, initial_node)
+    heapq.heappush(fringe, init_node)
 
     # As long as there are items in the fringe, keep searching for a solution
-    while fringe is not None:
+    while fringe:
+
         # Get the first node from the fringe
+
         node = heapq.heappop(fringe)
-        # print_map(node.state)
-        # print(node.get_manhattan_distance())
+
         # Check if the nodes state is equal to the goal state
-        if goal_reached(node.state, goal_positions):
+        if goal_reached(node, goal_positions):
             return node.path()
         # Find the children of the node and store them in the fringe
         children = expand(node, goal_positions, visited)
+
         for child in children:
-            child.set_manhattan_distance(calculate_manhattan_distance(find_crate_positions(child.state), goal_positions, child))
-            child.set_boxes_not_in_goal(calculate_boxes_not_in_goal(find_crate_positions(child.state), goal_positions, child))
+            child.set_manhattan_distance(
+                calculate_manhattan_distance(goal_positions, child))
+            child.set_boxes_not_in_goal(
+                calculate_boxes_not_in_goal(goal_positions, child))
             heapq.heappush(fringe, child)
 
-    length = len(fringe)
-    for i in range(0, length):
-        test = heapq.heappop(fringe)
-        print(test.get_manhattan_distance())
-        print(test.get_boxes_not_in_goal())
-        print_map(test.state)
 
-
-def expand(parent, goal_positions, visited):
+def expand(parent: Node, goal_positions, visited) -> list[Node]:
     """Expands the current node and returns a list of children (successors)"""
     children = []
-    actions = allowed_actions(parent.state)
+    actions = allowed_actions(parent)
+
     for action in actions:
-        parent_state = copy.deepcopy(parent.state)
-        next_state = do_action(parent_state, action, goal_positions)
-        child = Node(next_state, parent, action, parent.path_cost + 1)
+        next_robot, next_crates = do_action(parent, action, goal_positions)
+        child = Node(next_robot, next_crates, parent,
+                     action, parent.path_cost+1)
         if not is_visited(child, visited):
             children = insert_into(child, children)
     return children
 
 
-def calculate_manhattan_distance(crate_pos, goal_pos, node):
+def calculate_manhattan_distance(goal_pos, node: Node):
     distance = 0
-    for crate in crate_pos.values():
-        minimum_distance = sys.maxsize
-        x_crate, y_crate = crate
-        for goal in goal_pos.values():
-            x_goal, y_goal = goal
-            if node.state[x_goal][y_goal] == 'J':
+    for x_crate, y_crate in node.crate_positions:
+        minimum_distance = maxsize
+        for x_goal, y_goal in goal_pos:
+            if node.competition_map[x_goal][y_goal] == "J":
                 if x_crate != x_goal or y_crate != y_goal:
                     continue
-            manhattan_distance = abs(x_crate - x_goal) + abs(y_crate - y_goal)
+            manhattan_distance = abs(x_crate-x_goal)+abs(y_crate-y_goal)
             if manhattan_distance < minimum_distance:
                 minimum_distance = manhattan_distance
         distance += minimum_distance
@@ -123,177 +135,179 @@ def calculate_manhattan_distance(crate_pos, goal_pos, node):
     return distance
 
 
-def calculate_boxes_not_in_goal(crate_pos, goal_pos, node):
-    boxes_not_in_goal = len(find_crate_positions(node.state))
-    for crate in crate_pos.values():
-        for goal in goal_pos.values():
-            crate_x, crate_y = crate
-            goal_x, goal_y = goal
+def calculate_boxes_not_in_goal(goal_pos, node: Node):
+    boxes_not_in_goal = len(node.crate_positions)
+    for crate_x, crate_y in node.crate_positions:
+        for goal_x, goal_y in goal_pos:
             if crate_x == goal_x and crate_y == goal_y:
                 boxes_not_in_goal -= 1
+
     return boxes_not_in_goal
 
 
-def insert_into(node, queue):
+def insert_into(node, queue: list):
     """Inserts a node into the given queue/array"""
     queue.insert(0, node)
     return queue
 
 
-def insert_all(first_list, second_list):
-    """Inserts the first list into the second list"""
-    second_list.extend(first_list)
-    return second_list
-
-
-def remove_first(queue):
-    """Removes the first element of the fringe (queue/array)"""
-    return queue.pop(0)
-
-
-def allowed_actions(state):
+def allowed_actions(node: Node):
     """Returns a list of allowed actions from current state"""
     allowed = []
-    i, j = find_current_robot_pos(state)
-
-    if state[i - 1][j] == '.' or state[i - 1][j] == 'G':
-        allowed.append('u')
-    elif state[i - 1][j] == 'J' and state[i - 2][j] == '.' or state[i - 1][j] == 'J' and state[i - 2][j] == 'G':
+    i, j = node.robot_position
+    crates = node.crate_positions
+    if ((i - 1, j) in crates and node.competition_map[i - 2][j] == '.' or (i-1, j) in crates and node.competition_map[i - 2][j] == 'G') and (i-2, j) not in crates:
         allowed.append('U')
+    elif (node.competition_map[i - 1][j] == '.' or node.competition_map[i - 1][j] == 'G') and (i-1, j) not in crates:
+        allowed.append('u')
 
-    if state[i][j + 1] == '.':
-        allowed.append('r')
-    elif state[i][j + 1] == 'J' and state[i][j + 2] == '.' or state[i][j + 1] == 'J' and state[i][j + 2] == 'G':
+    if ((i, j+1) in crates and node.competition_map[i][j + 2] == '.' or (i, j+1) in crates and node.competition_map[i][j + 2] == 'G') and (i, j+2) not in crates:
         allowed.append('R')
+    elif (node.competition_map[i][j + 1] == '.' or node.competition_map[i][j+1] == 'G') and (i, j+1) not in crates:
+        allowed.append('r')
 
-    if state[i + 1][j] == '.':
-        allowed.append('d')
-    elif state[i + 1][j] == 'J' and state[i + 2][j] == '.' or state[i + 1][j] == 'J' and state[i + 2][j] == 'G':
+    if ((i+1, j) in crates and node.competition_map[i + 2][j] == '.' or (i+1, j) in crates and node.competition_map[i + 2][j] == 'G') and (i+2, j) not in crates:
         allowed.append('D')
+    elif (node.competition_map[i + 1][j] == '.' or node.competition_map[i + 1][j] == 'G') and (i+1, j) not in crates:
+        allowed.append('d')
 
-    if state[i][j - 1] == '.':
-        allowed.append('l')
-    elif state[i][j - 1] == 'J' and state[i][j - 2] == '.' or state[i][j - 1] == 'J' and state[i][j - 2] == 'G':
+    if ((i, j-1) in crates and node.competition_map[i][j - 2] == '.' or (i, j-1) in crates and node.competition_map[i][j - 2] == 'G') and (i, j-2) not in crates:
         allowed.append('L')
+    elif (node.competition_map[i][j - 1] == '.' or node.competition_map[i][j-1] == 'G') and (i, j-1) not in crates:
+        allowed.append('l')
 
     return allowed
 
 
-def do_action(state, action, goal_positions):
+def do_action(parent_node: Node, action: str, goal_positions):
     """Executes a given action on a given state, returning the resulting state
     MAKE THIS WORK WITH G AS WELL"""
-    i, j = find_current_robot_pos(state)
+    if len(action) > 1:
+        raise Exception(f"Argument 'action' was longer than 1 char, {action}")
+    i, j = parent_node.robot_position
+    new_crate_positions = parent_node.crate_positions.copy()
     # print('\n\nFound M at [' + str(i) + '][' + str(j) + '], now performing action: ' + action)
     pushing = True
     if action.islower():
         pushing = False
     if action.lower() == 'u':
         if pushing:
-            state[i - 2][j] = 'J'
-        state[i - 1][j] = 'M'
+            new_crate_pos = (i-2, j)
+            new_crate_positions.remove((i-1, j))
+            new_crate_positions.append(new_crate_pos)
+        new_robot_pos = (i-1, j)
     if action.lower() == 'r':
         if pushing:
-            state[i][j + 2] = 'J'
-        state[i][j + 1] = 'M'
+            new_crate_pos = (i, j+2)
+            new_crate_positions.remove((i, j+1))
+            new_crate_positions.append(new_crate_pos)
+        new_robot_pos = (i, j+1)
     if action.lower() == 'd':
         if pushing:
-            state[i + 2][j] = 'J'
-        state[i + 1][j] = 'M'
+            new_crate_pos = (i+2, j)
+            new_crate_positions.remove((i+1, j))
+            new_crate_positions.append(new_crate_pos)
+        new_robot_pos = (i+1, j)
     if action.lower() == 'l':
         if pushing:
-            state[i][j - 2] = 'J'
-        state[i][j - 1] = 'M'
-    if (i, j) in goal_positions.values():
-        state[i][j] = 'G'
-    else:
-        state[i][j] = '.'
-    return state
+            new_crate_pos = (i, j-2)
+            new_crate_positions.remove((i, j-1))
+            new_crate_positions.append(new_crate_pos)
+        new_robot_pos = (i, j-1)
+
+    new_crate_positions.sort()
+    return new_robot_pos, new_crate_positions
 
 
-def find_current_robot_pos(state):
-    i = 0
-    for line in state:
-        j = 0
-        for char in line:
-            if char == 'M':
-                return i, j
-            j += 1
-        i += 1
-    return -1, -1
-
-
-def find_crate_positions(state):
-    crate_positions = {}
-    i = 0
-    for line in state:
-        j = 0
-        for char in line:
-            if char == 'J':
-                crate_positions[len(crate_positions)] = i, j
-            j += 1
-        i += 1
-    return crate_positions
-
-
-def find_goal_positions(state):
-    goal_positions = {}
-    i = 0
-    for line in state:
-        j = 0
-        for char in line:
+def find_goal_positions(map):
+    goal_positions = []
+    for i, line in enumerate(map):
+        for j, char in enumerate(line):
             if char == 'G':
-                goal_positions[len(goal_positions)] = i, j
+                goal_positions.append((i, j))
             j += 1
         i += 1
-    return goal_positions
+    return tuple(goal_positions)
 
 
-def is_visited(node, visited):
+# def is_visited(node: Node, visited: list[Node]):
+#     if visited is not None:
+#         if node in visited:
+#             # for visited_node in visited:
+#             #     # the crate operation wont be equal for the same crates, cause the tuple is not sorted
+#             #     if visited_node.robot_position == node.robot_position and visited_node.crate_positions == node.crate_positions:
+#             #         print("is in visited")
+#             return True
+#     visited.append(node)
+#     return False
+
+def is_visited(node: Node, visited: dict[Node, int]):
     if visited is not None:
-        for visited_node in visited:
-            if find_current_robot_pos(visited_node.state) == find_current_robot_pos(
-                    node.state) and find_crate_positions(visited_node.state) == find_crate_positions(node.state):
-                return True
-    visited.append(node)
+        if visited.get(node) != None:
+            return True
+
+    visited[node] = 0
     return False
 
 
-def goal_reached(state, goal_positions):
+def goal_reached(node: Node, goal_positions):
     """Returns true if the current state is the goal state, otherwise false"""
-    lst = []
-    for goal in goal_positions.values():
-        i, j = goal
-        lst.append(state[i][j])
-    if all('J' in state for state in lst):
+    if all(x_y in node.crate_positions for x_y in goal_positions):
         return True
 
 
 def generate_map():
-    """Generates the initial map from a supplied text file and returns it as a 2D array"""
-    file = open("competition_map.txt")
-    outer_array = []
-    for line in file:
-        line.strip()
-        inner_array = []
-        for char in line:
-            inner_array.append(char)
-        outer_array.append(inner_array)
-    file.close()
-    return outer_array
+    with open("competition_map.txt") as file:
+        outer_array = []
+        robot_pos = None
+        crate_pos = []
+        for i, line in enumerate(file):
+            line = line.strip()
+            inner_array = []
+            for j, char in enumerate(line):
+                if char == "M":
+                    robot_pos = i, j
+                    inner_array.append(".")
+                    continue
+                elif char == "J":
+                    crate_pos.append((i, j))
+                    inner_array.append(".")
+                    continue
+                inner_array.append(char)
+            outer_array.append(inner_array)
+    if robot_pos is None:
+        raise Exception("Found no robot ('M' in map).")
+    return outer_array, robot_pos, crate_pos
 
 
-def print_map(state):
+def print_map(node: Node):
     """Prints the current state of the map"""
-    for array in state:
-        for char in array:
-            print(' ' + char, end='')
+    for i, array in enumerate(node.competition_map):
+        for j, char in enumerate(array):
+            if (i, j) == node.robot_position:
+                print(f" {print_red('M')}", end="")
+            elif (i, j) in node.crate_positions:
+                print(f" {print_green('J')}", end="")
+            else:
+                print(" "+char, end="")
+        print("\n")
     print('\n')
 
 
+def print_red(text: str):
+    assert isinstance(text, str)
+    return f"\033[0;91m{text}\033[00m"
+
+
+def print_green(text: str):
+    assert isinstance(text, str)
+    return f"\033[0;92m{text}\033[00m"
+
+
 def run():
-    path = search()
+    path: list[Node] = search()
     for node in path:
-        print_map(node.state)
+        print_map(node)
     print('Solution: ')
     for node in path[1:]:
         print(node.get_action(), end='')
